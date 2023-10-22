4210f83f20cd2b17e4eded524a1e144b44b05ce2
AndreySaez <theleji.andrey@gmail.com>
2023-10-22 21:38:48 +0400

    сделал xml разметку, написал логику для кнопок play и next

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var position = 0
+
+        val playBtn = findViewById<ImageView>(R.id.iv_start)
+        val nextBtn = findViewById<ImageView>(R.id.iv_next)
+        val previousBtn = findViewById<ImageView>(R.id.iv_previous)
+
+
+        val face = R.raw.face_salam
+        val queen = R.raw.queen_we_are_the_champions
+        val rick = R.raw.rick_astley_never_gonna_give_you_up
+        val shrek = R.raw.shrek
+
+
+        val musicList = arrayListOf(
+            face,
+            queen,
+            rick,
+            shrek,
+        )
+
+        val player: MediaPlayer = MediaPlayer.create(this, musicList[position])
+
+
+
+        playBtn.setOnClickListener {
+            if (!player.isPlaying) {
+                player.start()
+                playBtn.setImageResource(R.drawable.ic_stop)
+            } else {
+                player.pause()
+                playBtn.setImageResource(R.drawable.ic_start)
+            }
+        }
+
+        nextBtn.setOnClickListener {
+            if (player.isPlaying) {
+                player.stop()
+                player.reset()
+                val nextTrackFd =
+                    resources.openRawResourceFd(musicList[++position % musicList.size])
+                player.setDataSource(nextTrackFd)
+                player.prepare()
+                player.start()
+            }
+        }
+
+
     }
 }
